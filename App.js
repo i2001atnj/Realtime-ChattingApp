4d1599cf0df254d5c7d04d9899f8f5608debb614
Alejandro Torres Naranjo <124210642+i2001atnj@users.noreply.github.com>
2023-08-22 11:32:05 -0500

    changes

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { OnboardingScreen, LoginScreen, SignUpScreen } from './src/screens/screens';
+import {
+  OnboardingScreen,
+  LoginScreen,
+  SignUpScreen,
+} from "./src/screens/screens";
 
-import HomeStack from './src/navigation/HomeStack';
+import HomeStack from "./src/navigation/HomeStack";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,7 +17,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="light" />
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
